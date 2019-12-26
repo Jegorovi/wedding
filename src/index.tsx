12b89697641b4a960ec37303405155f32c884a82
Rolands Jegorovs <rolandjegorov@gmail.com>
2019-12-26 20:55:42 +0200

    Add easter egg, add proper translations

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,47 @@ import 'antd/dist/antd.css';
 import { App } from "./components/app/App";
 import * as serviceWorker from "./serviceWorker";
 
+/* eslint-disable no-useless-escape */
+console.log("%c LIVE LONG AND PROSPER! 🖖", "color: #f36b6b; font-weight: bold;")
+console.log(`
+                                      _
+                               mMm  _[_]_
+                              /(_)\\  (_)
+                             //)^(\\\\\//:\\\\
+                            /(/&@&\\)\\|~|/
+                           / /-~\`~-\\ |||
+                           \`/       \\|||
+                            \`-------'-'--
+                     ()@()@()@()@()@()@()@()@()@()
+                      }                         {
+                      }         S A R A         {
+                      }            &            {
+                      }       R O L A N D       {
+                      }                         {
+               ()@()@()@()@()@()@()@()@()@()@()@()@()@()
+                }         _  _           _  _         {
+                }        ( \\/ )         ( \\/ )        {
+                }         \\  /           \\  /         {
+                }          \\/             \\/          {
+                }                                     {
+         ()@()@()@()@()@()@()@()@()@()@()@()@()@()@()@()@()@()
+          }                                                 {
+          }                                                 {
+          }                                                 {
+          }                                                 {
+          }                                                 {
+          }                                                 {
+   ()@()@()@()@()@()@()@()@()@()@()@()@()@()@()@()@()@()@()@()@()@()
+    }                                                             {
+    }                                                             {
+    }                                                             {
+    }                                                             {
+    }                                                             {
+    }                                                             {
+   ()@()@()@()@()@()@()@()@()@()@()@()@()@()@()@()@()@()@()@()@()@()
+`);
+/* eslint-enable no-useless-escape */
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
