46296cad2a759cde11e60b528c075cba7e2d0533
Rolands Jegorovs <rolandjegorov@gmail.com>
2019-12-26 15:53:05 +0200

    Updates

---
+++ precache-manifest.76e7ba8754cd61eb53b369cd68ef54ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "719134dffbdb0f7ecc50fd41f504f2ea",
+    "url": "/wedding/index.html"
+  },
+  {
+    "revision": "ccf2bd581ed482c27786",
+    "url": "/wedding/static/css/2.aef8ad04.chunk.css"
+  },
+  {
+    "revision": "320d81f425a28d1ece95",
+    "url": "/wedding/static/css/main.3c535927.chunk.css"
+  },
+  {
+    "revision": "ccf2bd581ed482c27786",
+    "url": "/wedding/static/js/2.e9005118.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/wedding/static/js/2.e9005118.chunk.js.LICENSE"
+  },
+  {
+    "revision": "320d81f425a28d1ece95",
+    "url": "/wedding/static/js/main.91a39561.chunk.js"
+  },
+  {
+    "revision": "7dc0d570bb331ea4c74c",
+    "url": "/wedding/static/js/runtime-main.fdcf6201.js"
+  }
+]);
